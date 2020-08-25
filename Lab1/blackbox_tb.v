dcde51f087b7cb3801a54f14edda2fc89f029323
Chen, Haomiao <haomiao4@illinois.edu>
2020-08-25 15:23:56 -0500

    Update blackbox_tb.v

--- Lab1/blackbox_tb.v
+++ Lab1/blackbox_tb.v
@@ -1,3 +1,26 @@
 module blackbox_test;
+  reg z, k, r;
+	wire a;
+  blackbox b1(a, z, k, r);
+  initial begin
+		
+		  $dumpfile("blackbox.vcd");
+		  $dumpfile(0,blackbox_test);
+
+		  z=0; k=0; r=0; # 10;
+      z=0; k=0; r=1; # 10;
+      z=0; k=1; r=0; # 10;
+      z=0; k=1; r=1; # 10;
+      z=1; k=0; r=0; # 10;
+      z=1; k=0; r=1; # 10;
+      z=1; k=1; r=0; # 10;
+      z=1; k=1; r=1; # 10;
+		
+      $finish;
+  end
+	
+  initial
+		  $monitor("At time %2t, a = %d b = %d s = %d c = %d",
+                 $time, a, b, s, c); 
 
 endmodule // blackbox_test
