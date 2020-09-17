5557c12b13e8fa1583e25c136eaa2aff6ddf3c64
Haomiao Chen <haomiao4@linux-12.ews.illinois.edu>
2020-09-17 12:11:53 -0500

    Lab4 part1

--- Lab4/decoder.v
+++ Lab4/decoder.v
@@ -18,9 +18,9 @@ module mips_decode(rd_src, writeenable, alu_src2, alu_op, except, opcode, funct)
 	assign rd_src = !(opcode==`OP_OTHER0);
 	assign alu_src2[0] = opcode==`OP_ADDI;
 	assign alu_src2[1] = !((opcode ==`OP_OTHER0 | opcode ==`OP_ADDI));
-	assign alu_op[0] = (funct==`OP0_OR|opcode==`OP_ORI)|(funct==`OP0_XOR|opcode==`OP_XORI)|funct==`OP0_SUB;
-	assign alu_op[1] = !((funct==`OP0_AND|opcode==`OP_ANDI)|(funct==`OP0_OR|opcode==`OP_ORI));
-	assign alu_op[2] = !((funct==`OP0_ADD|opcode==`OP_ADDI)|funct==`OP0_SUB);
+	assign alu_op[0] = (opcode ==`OP_OTHER0 &(funct==`OP0_OR|funct==`OP0_XOR|funct==`OP0_SUB))|opcode==`OP_ORI|opcode==`OP_XORI;
+	assign alu_op[1] = ! ((opcode ==`OP_OTHER0 &(funct==`OP0_AND|funct==`OP0_OR))|opcode==`OP_ANDI|opcode==`OP_ORI);
+	assign alu_op[2] = ! ((opcode ==`OP_OTHER0 &(funct==`OP0_ADD|funct==`OP0_SUB))|opcode==`OP_ADDI);
 	
 	assign writeenable = !except;
 endmodule // mips_decode
