58bd64718abd818e90faa310f04c6d55c891e086
Haomiao Chen <haomiao4@linux-36.ews.illinois.edu>
2020-09-30 13:42:21 -0500

    Lab6 part1

--- Lab6/p1.s
+++ Lab6/p1.s
@@ -12,4 +12,28 @@
 # }
 .globl fill_array
 fill_array:
-    jr      $ra
+	move $t0, $a0                  #move request
+	move $t1, $a1                  #move array ptr
+	li   $t2, 0                    #i = 0
+	li   $t3, 3                    #load 3
+for:
+	bge  $t2, 6, return            #i<6
+	srl  $t0, $t0, 3               #right shift 
+if:
+	rem $t8, $t2, $t3              #divide
+	bne $t8, 0, else               #if remainder != 0
+	mul $t7, $t2, 4				   #i*4
+    add $t4, $t1, $t7              #calc offset
+	and $t6, $t0, 0x0000001f       #bitwise op
+	sw  $t6, 0($t4)                #store the value
+	add $t2, $t2, 1                #i++
+	j   for                        #back to for
+else:
+	mul $t7, $t2, 4				   #i*4
+    add $t4, $t1, $t7              #calc offset
+	and $t6, $t0, 0x000000ff       #bitwise op
+	sw  $t6, 0($t4)                #store the value
+	add $t2, $t2, 1                #i++
+	j   for                        #back to for
+return:
+	jr  $ra						   #return
