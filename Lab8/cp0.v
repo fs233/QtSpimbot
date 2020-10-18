64f1cbb6f6b61bfd2445ff1dde9248177dee1664
Haomiao Chen <haomiao4@linux-12.ews.illinois.edu>
2020-10-17 21:13:41 -0500

    Lab8

--- Lab8/cp0.v
+++ Lab8/cp0.v
@@ -14,4 +14,35 @@ module cp0(rd_data, EPC, TakenInterrupt,
     input         MTC0, ERET, TimerInterrupt, clock, reset;
 
     // your Verilog for coprocessor 0 goes here
+	wire   [31:0] user_status, decode0, status_register, cause_register, epc_register;
+	wire   [29:0] epc_d, epc_q;
+	wire          userenable, epcen, exreset, exception_level, epcenable, cs, ss, ns;
+	
+	register      ruser(user_status, wr_data, clock, userenable, reset);
+	decoder32     d0(decode0, regnum, MTC0);
+	assign        userenable = decode0[12];
+	assign        epcen = decode0[14];
+	or            o0(exreset, reset, ERET);
+	or            o1(epcenable, epcen, TakenInterrupt);
+	dffe          dex(exception_level, 1'h1, clock, TakenInterrupt, exreset);
+	mux2v #(30)   mepc(epc_d, wr_data[31:2], next_pc, TakenInterrupt);
+	register #(30, 0) repc(epc_q, epc_d, clock, epcenable, reset);
+	assign        EPC = epc_q;
+	assign        status_register[31:16] = 16'h0;
+	assign        status_register[15:8] = user_status[15:8];
+	assign        status_register[7:2] = 6'h0;
+	assign        status_register[1] = exception_level;
+	assign        status_register[0] = user_status[0];
+	assign        cause_register[31:16] = 16'h0;
+	assign        cause_register[15] = TimerInterrupt;
+	assign        cause_register[14:0] = 15'h0;
+	assign        epc_register[31:2] = epc_q;
+	assign        epc_register[1:0] = 2'b0;
+	mux32v        mrd(rd_data, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, status_register, cause_register, epc_register, 32'h0, 32'h0, 
+                  32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, regnum);
+	and           a0(cs, cause_register[15], status_register[15]);
+	not           no(ns, status_register[1]);
+	and           a1(ss, ns, status_register[0]);
+	and           a2(TakenInterrupt, cs, ss);
+	
 endmodule
--- Lab8/machine.v
+++ Lab8/machine.v
@@ -18,9 +18,12 @@ module machine(clk, reset);
    wire [31:0]  rd1_data, rd2_data, B_data, alu_out_data, load_data, wr_data;
 
    //Your extra wires go here
+   wire [31:0]  rd_data, wr_dataf, handleaddr;
+   wire [29:0]  EPC, nnext_PC, fnext_PC;
+   wire         TakenInterrupt, TimerInterrupt, TimerAddress, NotIO, MemReadin, MemWritein;
 
 
-   register #(30, 30'h100000) PC_reg(PC[31:2], next_PC[31:2], clk, /* enable */1'b1, reset);
+   register #(30, 30'h100000) PC_reg(PC[31:2], fnext_PC, clk, /* enable */1'b1, reset);
    assign PC[1:0] = 2'b0;  // bottom bits hard coded to 00
    adder30 next_PC_adder(PC_plus4, PC[31:2], 30'h1);
    adder30 target_PC_adder(PC_target, PC_plus4, imm[29:0]);
@@ -29,22 +32,26 @@ module machine(clk, reset);
 
    instruction_memory imem (inst, PC[31:2]);
 
-   mips_decode decode(ALUOp, RegWrite, BEQ, ALUSrc, MemRead, MemWrite, MemToReg, RegDst, MFC0, MTC0, ERET,
-                      inst);
+   mips_decode decode(ALUOp, RegWrite, BEQ, ALUSrc, MemRead, MemWrite, MemToReg, RegDst, MFC0, MTC0, ERET, inst);
 
-   regfile rf (rd1_data, rd2_data,
-               rs, rt, wr_regnum, wr_data,
-               RegWrite, clk, reset);
+   regfile rf (rd1_data, rd2_data, rs, rt, wr_regnum, wr_dataf, RegWrite, clk, reset);
 
    mux2v #(32) imm_mux(B_data, rd2_data, imm, ALUSrc);
    alu32 alu(alu_out_data, zero, negative, ALUOp, rd1_data, B_data);
 
-   data_mem data_memory(load_data, alu_out_data, rd2_data, MemRead, MemWrite, clk, reset);
+   data_mem data_memory(load_data, alu_out_data, rd2_data, MemReadin, MemWritein, clk, reset);
 
    mux2v #(32) wb_mux(wr_data, alu_out_data, load_data, MemToReg);
    mux2v #(5) rd_mux(wr_regnum, rt, rd, RegDst);
    
    //Connect your new modules below
-   
-
+   timer t0(TimerInterrupt, load_data, TimerAddress, rd2_data, alu_out_data, MemRead, MemWrite, clk, reset);
+   cp0  c0(rd_data, EPC, TakenInterrupt, rd2_data, rd, next_PC, MTC0, ERET, TimerInterrupt, clk, reset);
+	not  n0(NotIO, TimerAddress);
+	and  a0(MemReadin, MemRead, NotIO);
+	and  a1(MemWritein, MemWrite, NotIO);
+	mux2v #(32) wd_mux(wr_dataf, wr_data, rd_data, MFC0);
+	mux2v #(30) newb_mux(nnext_PC, next_PC, EPC, ERET);
+	assign handleaddr = 32'h80000180;
+	mux2v #(30) fpc_mux(fnext_PC, nnext_PC, handleaddr[31:2], TakenInterrupt);
 endmodule // machine
